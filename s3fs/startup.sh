14be9da2c5ecb8f3022204a02b90f96f41626391
Dragos Costache <dragos.costache@infinityworks.com>
2020-09-17 14:18:47 +0100

    Refactor to perform cleanup on container exit

--- s3fs/startup.sh
+++ s3fs/startup.sh
@@ -1,53 +1,36 @@
 #!/bin/sh
 
-set -eu
-
-mkdir -p /mnt/s3fs/s3-home && mkdir -p /mnt/s3fs/s3-shared
-
+set -eux
 
 # Mount entire S3 bucket with home KMS key and set permission metadata for user home dir
 
 mkdir -p /mnt/tmp
 
-/opt/s3fs-fuse/bin/s3fs ${S3_BUCKET} /mnt/tmp -f \
+/opt/s3fs-fuse/bin/s3fs ${S3_BUCKET} /mnt/tmp \
     -o ecs \
     -o endpoint=eu-west-2 \
     -o url=https://s3.amazonaws.com \
-    -o use_sse=kmsid:${KMS_HOME} &>/dev/null &
-
-S3FS_PID=$!
-
-while [ -z "$( mount | grep ^s3fs  )" ]; do
-    echo "Waiting for mount";
-    sleep 1;
-done
+    -o use_sse=kmsid:${KMS_HOME}
 
 chmod -R a-r /mnt/tmp/home/${USER} # || true Needed as this will fail if there are some files encrypted with a different KMS key
 
 fusermount -u /mnt/tmp
-kill -9 $S3FS_PID || true
 
 # Mount entire S3 bucket with shared KMS key and set permission metadata for shared dir
 
-/opt/s3fs-fuse/bin/s3fs ${S3_BUCKET} /mnt/tmp -f \
+/opt/s3fs-fuse/bin/s3fs ${S3_BUCKET} /mnt/tmp \
     -o ecs \
     -o endpoint=eu-west-2 \
     -o url=https://s3.amazonaws.com \
-    -o use_sse=kmsid:${KMS_SHARED} &>/dev/null &
-
-S3FS_PID=$!
-
-while [ -z "$( mount | grep ^s3fs  )" ]; do
-    echo "Waiting for mount";
-    sleep 1;
-done
+    -o use_sse=kmsid:${KMS_SHARED}
 
 chmod -R a-r /mnt/tmp/shared || true # || true Needed as this will fail if there are some files encrypted with a different KMS key
 
 fusermount -u /mnt/tmp
-kill -9 $S3FS_PID || true
 
 rm -rf /mnt/tmp
+
+mkdir -p /mnt/s3fs/s3-home && mkdir -p /mnt/s3fs/s3-shared
 
 nohup /opt/s3fs-fuse/bin/s3fs ${S3_BUCKET}:/home/${USER} /mnt/s3fs/s3-home -f \
     -o allow_other \
@@ -63,4 +46,14 @@ nohup /opt/s3fs-fuse/bin/s3fs ${S3_BUCKET}:/shared /mnt/s3fs/s3-shared -f \
     -o url=https://s3.amazonaws.com \
     -o use_sse=kmsid:${KMS_SHARED} &> /var/log/s3fs-shared &
 
-tail -f /var/log/s3fs-home -f /var/log/s3fs-shared
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    fusermount -u /mnt/s3fs/s3-home
+    fusermount -u /mnt/s3fs/s3-shared
+    rm -rf /mnt/s3fs/*
+}
+
+trap 'cleanup' SIGTERM
+
+tail -f /var/log/s3fs-home -f /var/log/s3fs-shared &
+wait $!
