b29e11dbe97b80fa910c1eb964eaa8cefade7c4b
thomasstock-caldwell <thomas.stock-caldwell@engineering.digital.dwp.gov.uk>
2021-03-15 10:18:48 +0000

    Add s3fs home loaction as data-dir for mariadb

--- hue/startup.sh
+++ hue/startup.sh
@@ -4,10 +4,12 @@ cat /tmp/hue-overrides.ini | envsubst > ./desktop/conf/hue-overrides.ini
 
 /usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /usr/share/hue/key.pem -out /usr/share/hue/cert.pem -days 30 -nodes -subj '/CN=hue'
 
-mariadb-install-db --user=hue --datadir=/usr/share/hue/data --skip-test-db --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880
-echo Preparing to start DB server
-sleep 5
-mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/usr/share/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
+if [ ! -d /mnt/s3fs/s3-home/hue/data ]; then
+  mariadb-install-db --user=hue --datadir=/mnt/s3fs/s3-home/hue/data --skip-test-db --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880
+  echo Preparing to start DB server
+  sleep 5
+fi
+mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/mnt/s3fs/s3-home/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
 echo Preparing to reset DB password
 sleep 5
 mariadb --socket=/tmp/maria.sock  << !EOF
