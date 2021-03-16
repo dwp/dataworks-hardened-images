1bb5833b4c0db605ac12879270442d3c00935dc4
Thomas Caldwell <caldwellt@Thomass-MBP.lan>
2021-03-16 17:32:35 +0000

    Add required steps to startup.sh to use existing hue data from s3-home

--- hue/startup.sh
+++ hue/startup.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+save_session_data() {
+  echo "Container shutting down, saving session data now..."
+  mariadb-dump --socket=/tmp/maria.sock --databases hue > /tmp/huedb_backup.sql || return
+  mkdir -p /mnt/s3fs/s3-home/.huedb
+  mv /tmp/huedb_backup.sql /mnt/s3fs/s3-home/.huedb/huedb_session_dump.sql
+}
+
+trap save_session_data SIGTERM
+
 cat /tmp/hue-overrides.ini | envsubst > ./desktop/conf/hue-overrides.ini
 
 /usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /usr/share/hue/key.pem -out /usr/share/hue/cert.pem -days 30 -nodes -subj '/CN=hue'
 
-if [ ! -d /mnt/s3fs/s3-home/hue/data ]; then
-  mariadb-install-db --user=hue --datadir=/mnt/s3fs/s3-home/hue/data --skip-test-db --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880
-  echo Preparing to start DB server
-  sleep 5
-fi
-mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/mnt/s3fs/s3-home/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
+mariadb-install-db --user=hue --datadir=/usr/share/hue/data --skip-test-db --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880
+echo Preparing to start DB server
+sleep 5
+
+mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/usr/share/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
 echo Preparing to reset DB password
 sleep 5
 mariadb --socket=/tmp/maria.sock  << !EOF
@@ -19,7 +27,17 @@ mariadb --socket=/tmp/maria.sock  << !EOF
     QUIT
 !EOF
 
+if [ -f "/mnt/s3fs/s3-home/.huedb/huedb_session_dump.sql" ]; then
+  echo restoring previous session data from huedb sql dump file...
+  mariadb --socket=/tmp/maria.sock hue < /mnt/s3fs/s3-home/.huedb/huedb_session_dump.sql
+else
+  echo previous session sql file does not exist, not running mysql query to restore huedb...
+fi
+
 ./build/env/bin/hue migrate
 ./build/env/bin/hue createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME:-hue} --email ${DJANGO_SUPERUSER_EMAIL:-hue@example.org}
-./build/env/bin/supervisor
+./build/env/bin/supervisor &
 
+while true; do
+  sleep 1
+done
