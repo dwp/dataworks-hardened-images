4a25da34c273a2de06a514ef9193c1244aad6967
Thomas Caldwell <caldwellt@Thomass-MBP.lan>
2021-03-16 17:32:35 +0000

    Add required steps to startup.sh to use existing hue data from s3-home

--- hue/startup.sh
+++ hue/startup.sh
@@ -8,18 +8,25 @@ if [ ! -d /mnt/s3fs/s3-home/hue/data ]; then
   mariadb-install-db --user=hue --datadir=/mnt/s3fs/s3-home/hue/data --skip-test-db --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880
   echo Preparing to start DB server
   sleep 5
-fi
-mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/mnt/s3fs/s3-home/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
-echo Preparing to reset DB password
-sleep 5
-mariadb --socket=/tmp/maria.sock  << !EOF
-    CREATE DATABASE hue COLLATE = 'utf8_general_ci';
-    GRANT ALL ON hue.* TO 'hue'@'%' IDENTIFIED BY 'notsecret';
-    SELECT * FROM INFORMATION_SCHEMA.SCHEMATA;
-    QUIT
+
+  mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/mnt/s3fs/s3-home/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
+  echo Preparing to reset DB password
+  sleep 5
+  mariadb --socket=/tmp/maria.sock  << !EOF
+      CREATE DATABASE hue COLLATE = 'utf8_general_ci';
+      GRANT ALL ON hue.* TO 'hue'@'%' IDENTIFIED BY 'notsecret';
+      SELECT * FROM INFORMATION_SCHEMA.SCHEMATA;
+      QUIT
 !EOF
 
-./build/env/bin/hue migrate
-./build/env/bin/hue createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME:-hue} --email ${DJANGO_SUPERUSER_EMAIL:-hue@example.org}
-./build/env/bin/supervisor
+  ./build/env/bin/hue migrate
+  ./build/env/bin/hue createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME:-hue} --email ${DJANGO_SUPERUSER_EMAIL:-hue@example.org}
+  ./build/env/bin/supervisor
+else
+  echo users hue data found in s3-home location...
 
+  mariadbd --user=hue --socket=/tmp/maria.sock --datadir=/mnt/s3fs/s3-home/hue/data --innodb-log-file-size=10000000 --innodb-buffer-pool-size=5242880 &
+  sleep 5
+  ./build/env/bin/hue migrate
+  ./build/env/bin/supervisor
+fi
