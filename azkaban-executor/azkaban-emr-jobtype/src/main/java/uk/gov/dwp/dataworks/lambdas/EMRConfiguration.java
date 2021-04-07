ba9fc60045c7167d1a684017a679e2a02b856edf
ian.utley <ian.utley@infinityworks.com>
2021-04-07 16:37:46 +0100

    Allow override of cluster name via job properties

--- azkaban-executor/azkaban-emr-jobtype/src/main/java/uk/gov/dwp/dataworks/lambdas/EMRConfiguration.java
+++ azkaban-executor/azkaban-emr-jobtype/src/main/java/uk/gov/dwp/dataworks/lambdas/EMRConfiguration.java
@@ -1,6 +1,6 @@
 package uk.gov.dwp.dataworks.lambdas;
 
-import org.codehaus.jackson.annotate.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class EMRConfiguration {
     private Overrides overrides;
