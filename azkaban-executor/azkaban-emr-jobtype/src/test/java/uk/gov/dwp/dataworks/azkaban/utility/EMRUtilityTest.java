c3aecaabae30dda1681ea05ce0c1fdf3d47333b9
Daniel Chicot <dan.chicot@gmail.com>
2021-04-29 16:40:19 +0100

    Cancel fixes.
    On cancelling a job do not only get the list of cluster ids on condition
    that the job has been killed (because it will have been) and do not kill
    the azkaban process because it can only ever be null.

--- azkaban-executor/azkaban-emr-jobtype/src/test/java/uk/gov/dwp/dataworks/azkaban/utility/EMRUtilityTest.java
+++ azkaban-executor/azkaban-emr-jobtype/src/test/java/uk/gov/dwp/dataworks/azkaban/utility/EMRUtilityTest.java
@@ -4,11 +4,13 @@ import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
 import com.amazonaws.services.elasticmapreduce.model.ClusterSummary;
 import com.amazonaws.services.elasticmapreduce.model.ListClustersRequest;
 import com.amazonaws.services.elasticmapreduce.model.ListClustersResult;
+import groovy.lang.IntRange;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -19,15 +21,19 @@ import static org.mockito.Mockito.*;
 class EMRUtilityTest {
 
     @Test
+    void activeClusterIdReturnsOptionalOfIdWhenFound() {
+        IntStream.range(0, 25).forEach(this::verifyActiveCluster);
+    }
+
+    @Test
+    void activeClusterIdReturnsEmptyOptionalWhenNotFound() {
+        IntStream.range(0, 25).forEach(this::verifyClusterNotFound);
+    }
+
+    @Test
     void activeClusterSummariesReturnsAllPaginatedResults() {
-        ListClustersResult page1 = clustersResult(1, 0, 10);
-        ListClustersResult page2 = clustersResult(2, 10, 20);
-        ListClustersResult page3 = clustersResult(null, 20, 25);
-        AmazonElasticMapReduce emr = mock(AmazonElasticMapReduce.class);
-        when(emr.listClusters(any())).thenReturn(page1, page2, page3);
-
+        AmazonElasticMapReduce emr = emrClient();
         List<ClusterSummary> summaries = EMRUtility.activeClusterSummaries(emr);
-
         ArgumentCaptor<ListClustersRequest> requestCaptor = ArgumentCaptor.forClass(ListClustersRequest.class);
         assertEquals(25, summaries.size());
         IntStream.range(0, 25).forEach(x -> assertEquals(clusterId(x), summaries.get(x).getId()));
@@ -39,12 +45,37 @@ class EMRUtilityTest {
         assertEquals(marker(2), requests.get(2).getMarker());
     }
 
+    private AmazonElasticMapReduce emrClient() {
+        ListClustersResult page1 = clustersResult(1, 0, 10);
+        ListClustersResult page2 = clustersResult(2, 10, 20);
+        ListClustersResult page3 = clustersResult(null, 20, 25);
+        AmazonElasticMapReduce emr = mock(AmazonElasticMapReduce.class);
+        when(emr.listClusters(any())).thenReturn(page1, page2, page3);
+        return emr;
+    }
+
     private ListClustersResult clustersResult(Integer marker, int startId, int endId) {
         ListClustersResult page1 = mock(ListClustersResult.class);
         when(page1.getMarker()).thenReturn(marker(marker));
         List<ClusterSummary> page1Summaries = paginatedSummaries(startId, endId);
         when(page1.getClusters()).thenReturn(page1Summaries);
         return page1;
+    }
+
+    private void verifyActiveCluster(int x) {
+        verifyClusterId("CLUSTER-" + x, "J-" + x, "NOT FOUND " + x);
+    }
+
+    private void verifyClusterNotFound(int x) {
+        verifyClusterId("NON_ACTIVE_CLUSTER-" + x, "NOT FOUND " + x, "NOT FOUND " + x);
+    }
+
+
+    private void verifyClusterId(String clusterName, String expected, String optionalElse) {
+        AmazonElasticMapReduce emr = emrClient();
+        Optional<String> clusterId = EMRUtility.activeClusterId(emr, clusterName);
+        String id = clusterId.orElse(optionalElse);
+        assertEquals(expected, id);
     }
 
     private List<ClusterSummary> paginatedSummaries(int startId, int endId) {
@@ -54,11 +85,16 @@ class EMRUtilityTest {
     private ClusterSummary clusterSummary(int x) {
         ClusterSummary summary = mock(ClusterSummary.class);
         when(summary.getId()).thenReturn(clusterId(x));
+        when(summary.getName()).thenReturn(clusterName(x));
         return summary;
     }
 
-    private String clusterId(int x) {
+    private String clusterName(int x) {
         return "CLUSTER-" + x;
+    }
+
+    private String clusterId(int x) {
+        return "J-" + x;
     }
 
     private String marker(Integer x) {
