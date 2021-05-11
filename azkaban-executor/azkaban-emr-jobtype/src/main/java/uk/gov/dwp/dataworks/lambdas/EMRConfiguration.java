88d480a2a74a422bf06a9fc307b1f95abc19b2ae
MattToczek <toczek.matt@gmail.com>
2021-05-11 14:33:04 +0100

    adding ability to pass in s3 location override to EMR launcher, using Azkaban

--- azkaban-executor/azkaban-emr-jobtype/src/main/java/uk/gov/dwp/dataworks/lambdas/EMRConfiguration.java
+++ azkaban-executor/azkaban-emr-jobtype/src/main/java/uk/gov/dwp/dataworks/lambdas/EMRConfiguration.java
@@ -1,9 +1,18 @@
 package uk.gov.dwp.dataworks.lambdas;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
 public class EMRConfiguration {
     private Overrides overrides;
+
+    private S3Overrides s3Overrides;
+
+    public EMRConfiguration(String name, String config){
+        this.overrides = new Overrides(name);
+        this.s3Overrides = new S3Overrides(config);
+    }
 
     public EMRConfiguration(String name){
         this.overrides = new Overrides(name);
@@ -12,21 +21,34 @@ public class EMRConfiguration {
     public static EMRConfigurationBuilder builder() {
         return new EMRConfigurationBuilder();
     }
-
     public Overrides getOverrides() {
         return overrides;
     }
 
+    @JsonProperty("s3_overrides")
+    public S3Overrides getS3Overrides() { return s3Overrides; }
+
+
     public static class EMRConfigurationBuilder {
         private String name;
+        private String config;
 
         public EMRConfigurationBuilder withName(String name) {
             this.name = name;
             return this;
         }
 
+        public EMRConfigurationBuilder withS3Overrides(String config) {
+            this.config = config;
+            return this;
+        }
+
         public EMRConfiguration build() {
-            return new EMRConfiguration(this.name);
+            if (this.config != null){
+                return new EMRConfiguration(this.name, this.config);
+            } else {
+                return new EMRConfiguration(this.name);
+            }
         }
     }
 
@@ -41,6 +63,20 @@ public class EMRConfiguration {
         @JsonProperty("Name")
         public String getName() {
             return this.name;
+        }
+    }
+
+    public static class S3Overrides {
+
+        private String config;
+
+        public S3Overrides(String config) {
+            this.config = config;
+        }
+
+        @JsonProperty("emr_launcher_config_s3_folder")
+        public String getConfig() {
+            return this.config;
         }
     }
 }
--- azkaban-executor/azkaban-emr-jobtype/src/test/java/uk/gov/dwp/dataworks/lambdas/EMRConfigurationTest.java
+++ azkaban-executor/azkaban-emr-jobtype/src/test/java/uk/gov/dwp/dataworks/lambdas/EMRConfigurationTest.java
@@ -3,6 +3,7 @@ package uk.gov.dwp.dataworks.lambdas;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.IOException;
 
@@ -16,10 +17,26 @@ class EMRConfigurationTest {
     }
 
     @Test
-    public void hasCorrectJsonStructure() throws IOException {
+    public void hasCorrectJsonStructureNameOnly() throws IOException {
         EMRConfiguration test = EMRConfiguration.builder().withName("test_name").build();
         ObjectMapper mapper = new ObjectMapper();
         String event = mapper.writeValueAsString(test);
         assertEquals("{'overrides':{'Name':'test_name'}}".replaceAll("'", "\""), event);
+    }
+
+    @Test
+    public void hasCorrectJsonStructureNameAndConfigLocation() throws IOException {
+        EMRConfiguration test = EMRConfiguration.builder().withName("test_name").withS3Overrides("test/config.yml").build();
+        ObjectMapper mapper = new ObjectMapper();
+        String event = mapper.writeValueAsString(test);
+        try {
+
+            JsonNode jsonNode = mapper.readValue(event, JsonNode.class);
+            assertEquals("{'Name':'test_name'}", jsonNode.get("overrides").asText());
+            assertEquals("{'emr_launcher_config_s3_folder':'test/config.yml'}", jsonNode.get("s3_overrides").asText());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
