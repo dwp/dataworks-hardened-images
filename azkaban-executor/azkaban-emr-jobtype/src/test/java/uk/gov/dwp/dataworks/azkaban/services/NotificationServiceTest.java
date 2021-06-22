af391dac7361ad3c1fa42c5c8c565d80819c2773
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-22 08:38:25 +0100

    Missing import

--- azkaban-executor/azkaban-emr-jobtype/src/test/java/uk/gov/dwp/dataworks/azkaban/services/NotificationServiceTest.java
+++ azkaban-executor/azkaban-emr-jobtype/src/test/java/uk/gov/dwp/dataworks/azkaban/services/NotificationServiceTest.java
@@ -19,7 +19,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 class NotificationServiceTest {
