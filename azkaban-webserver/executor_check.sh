2b4632b56c74bf146452ea954b6c69fedd590378
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-08-13 10:35:00 +0100

    fix remove
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -7,7 +7,7 @@ for executor_host in $(cat executors.list);
 do
     if [[ $(nc -v -z -w5 $executor_host 7082 && echo $?) != 0 ]]; then
         echo "$executor_host appears to be down, removing from active list..."
-        mysql $DB_NAME < DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\'; >
+        mysql $DB_NAME -e DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\';
     else
         echo "$executor_host available"
     fi
