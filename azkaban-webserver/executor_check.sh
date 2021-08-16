cace77e4bedce232d3bb055294467851b01f18db
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-08-16 08:54:40 +0100

    More verbose check
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -e
 
-mysql $DB_NAME -e < SELECT DISTINCT host FROM $DB_NAME.executors; > executors.list
+echo "Obtaining executor list..."\n
+mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD -e < SELECT DISTINCT host FROM $DB_NAME.executors; > /executors.list
 
-for executor_host in $(cat executors.list);
+echo "Current Executors: "\n
+cat /executors.list
+
+for executor_host in $(cat /executors.list);
 do
     if [[ $(nc -v -z -w5 $executor_host 7082 && echo $?) != 0 ]]; then
         echo "$executor_host appears to be down, removing from active list..."
-        mysql $DB_NAME -e DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\';
+        mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD -e DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\';
     else
         echo "$executor_host available"
     fi
