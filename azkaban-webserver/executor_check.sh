0108b2be068884c13334b9e3ea229dbe43d8f6cc
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-23 14:55:59 +0100

    Fix password parameter
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -12,7 +12,7 @@ for executor_host in $(tail -n +2 /executors.list);
 do
     if [[ $(nc -v -z -w5 $executor_host 7082 && echo $?) != 0 ]]; then
         echo "$executor_host appears to be down, removing from active list..."
-        mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD $DB_NAME -e "DELETE FROM $DB_NAME.executors WHERE host LIKE '${executor_host}';"
+        mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e "DELETE FROM $DB_NAME.executors WHERE host LIKE '${executor_host}';"
     else
         echo "$executor_host available"
     fi
