b98d35f997bdf51212d2424ef020ad7e751b35d0
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-23 11:33:20 +0100

    Fix query
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "Obtaining executor list...\n"
 echo "DB HOST ${DB_HOST}"
-echo "DB USERNAME ${DB_USERNAME}"
+echo "DB USERNAME ${DB_USERNAME}"azkaban-database.cluster-cg4gggiy7hda.eu-west-2.rds.amazonaws.com
 echo "DB NAME ${DB_NAME}"
-mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e SELECT DISTINCT host FROM $DB_NAME.executors; > /executors.list
+mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e "SELECT DISTINCT host FROM $DB_NAME.executors;" > /executors.list
 
 echo "Current Executors: "\n
 cat /executors.list
@@ -14,7 +14,7 @@ for executor_host in $(cat /executors.list);
 do
     if [[ $(nc -v -z -w5 $executor_host 7082 && echo $?) != 0 ]]; then
         echo "$executor_host appears to be down, removing from active list..."
-        mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD $DB_NAME -e DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\';
+        mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD $DB_NAME -e "DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\';"
     else
         echo "$executor_host available"
     fi
