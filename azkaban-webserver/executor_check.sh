ca975b5e38584c94f7aa1aa5ff3610b568d1c1f7
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-23 11:16:11 +0100

    Add DB_NAME to mysql command
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Obtaining executor list..."\n
-mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD -e SELECT DISTINCT host FROM $DB_NAME.executors; > /executors.list
+mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e SELECT DISTINCT host FROM $DB_NAME.executors; > /executors.list
 
 echo "Current Executors: "\n
 cat /executors.list
@@ -11,7 +11,7 @@ for executor_host in $(cat /executors.list);
 do
     if [[ $(nc -v -z -w5 $executor_host 7082 && echo $?) != 0 ]]; then
         echo "$executor_host appears to be down, removing from active list..."
-        mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD -e DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\';
+        mysql -h $DB_HOST -u $DB_USERNAME -p $DB_PASSWORD $DB_NAME -e DELETE FROM $DB_NAME.executors WHERE host LIKE \'${executor_host}\';
     else
         echo "$executor_host available"
     fi
