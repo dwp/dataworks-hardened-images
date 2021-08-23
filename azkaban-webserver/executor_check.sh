3aa1c71144347a6c412fbe1b1ca5104b5b4216a7
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-23 11:52:01 +0100

    For for loop over hosts in executors file
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
+<<<<<<< Updated upstream
 echo "Obtaining executor list...\n"
 mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e "SELECT DISTINCT host FROM $DB_NAME.executors;" > /executors.list
 
-echo "Current Executors: "\n
+echo "Current Executors:"
 cat /executors.list
 
-for executor_host in $(cat /executors.list);
+for executor_host in $(tail -n +2 /executors.list);
 do
     if [[ $(nc -v -z -w5 $executor_host 7082 && echo $?) != 0 ]]; then
         echo "$executor_host appears to be down, removing from active list..."
