d3610130bcd2d0096e68033b0c5bf234baac2ba2
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-25 12:02:29 +0100

    Give executors 3 chances to be alive
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 echo "Obtaining executor list..."
 mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e "SELECT DISTINCT host FROM $DB_NAME.executors;" > /executors.list
 
@@ -8,23 +7,28 @@ echo "Current Executors:"
 cat /executors.list
 
 # Executors list contains 'host' mysql header - ignore first line
-for executor_host in $(tail -n +2 /executors.list);
-do
+for executor_host in $(tail -n +2 /executors.list); do
   killed_instances=0
+  attempts=1
   echo "Executor host '${executor_host}'"
   echo "Executor port '${executor_port}'"
 
-    if [[ $(nc -v -z -w5 $executor_host $EXECUTOR_PORT && echo $?) != 0 ]]; then
-        echo "$executor_host appears to be down, removing from active list..."
+  while [[ $((nc -v -z -w5 $executor_host $EXECUTOR_PORT && echo $?)) != 0 && $attempts <= 3 ]]; do
+      if [[ $attempts = 3 ]]; then
         mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e "DELETE FROM $DB_NAME.executors WHERE host LIKE '${executor_host}';"
         (( killed_instances++ ))
-    else
-        echo "$executor_host available"
-    fi
+      else
+        echo "$executor_host failed to connect. Attempt '$attempts'. Retrying..."
+        (( attempts ++ ))
+        sleep 5
+      fi
+  done
+
+  echo "Successfully connected to '$executor_host' on attempt '$attempts'"
+done
 
 #    Kill the container after clearing all dead hosts - required because Azkaban doesn't read database after starting
-  if [[ "${killed_instances}" != 0 ]]; then
-    echo "Killing container after clearing dead executor hosts from database"
-    pkill -9 java
-  fi
-done
+if [[ "${killed_instances}" != 0 ]]; then
+  echo "Killing container after clearing dead executor hosts from database"
+  pkill -9 java
+fi
