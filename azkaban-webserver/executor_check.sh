581cadab7de0a3ab15e48c712884891552100eca
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-08-25 13:44:26 +0100

    Fix executor connect checks (#153)
    * Fix executor health check script

--- azkaban-webserver/executor_check.sh
+++ azkaban-webserver/executor_check.sh
@@ -8,23 +8,32 @@ echo "Current Executors:"
 cat /executors.list
 
 # Executors list contains 'host' mysql header - ignore first line
-for executor_host in $(tail -n +2 /executors.list);
-do
+for executor_host in $(tail -n +2 /executors.list); do
   killed_instances=0
+  attempts=1
   echo "Executor host '${executor_host}'"
   echo "Executor port '${executor_port}'"
 
-    if [[ $(nc -v -z -w5 $executor_host $EXECUTOR_PORT && echo $?) != 0 ]]; then
-        echo "$executor_host appears to be down, removing from active list..."
+  while [[ $(nc -v -z -w5 $executor_host $executor_port && echo $?) != 0 ]] && [[ $attempts -le 3 ]]; do
+      if [[ $attempts = 3 ]]; then
         mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME -e "DELETE FROM $DB_NAME.executors WHERE host LIKE '${executor_host}';"
+        (( attempts ++ ))
         (( killed_instances++ ))
-    else
-        echo "$executor_host available"
-    fi
+        break
+      else
+        echo "$executor_host failed to connect on attempt '$attempts'. Retrying..."
+        (( attempts ++ ))
+        sleep 5
+      fi
+  done
 
-#    Kill the container after clearing all dead hosts - required because Azkaban doesn't read database after starting
-  if [[ "${killed_instances}" != 0 ]]; then
-    echo "Killing container after clearing dead executor hosts from database"
-    pkill -9 java
+  if [[ $attempts -lt 3 ]]; then
+    echo "Successfully connected to '$executor_host' on attempt '$attempts'"
   fi
 done
+
+#    Kill the container after clearing all dead hosts - required because Azkaban doesn't read database after starting
+if [[ "${killed_instances}" != 0 ]]; then
+  echo "Killing container after clearing dead executor hosts from database"
+  pkill -9 java
+fi
