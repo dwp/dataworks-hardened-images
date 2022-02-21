52353d1ec6e45182918f125b051eaf860b5e5105
tobywalls <60394163+tobywalls@users.noreply.github.com>
2022-02-21 16:37:00 +0000

    prelim change to log user in 401 responses (#169)
    * prelim change to log user in 401 responses
    
    * bump openresty version

--- livy-proxy/authenticate.lua
+++ livy-proxy/authenticate.lua
@@ -2,7 +2,10 @@ local req_headers = ngx.req.get_headers()
 local token = (req_headers["Authorization"]
             and req_headers["Authorization"]
             or ngx.var.arg_token)
+local user = (req_headers["Username"]
+            and req_headers["Username"]
+            or "NOT_SET")
 
-local jwt = check_jwt(token)
+local jwt = check_jwt(token, user)
 substitute_proxy_user(jwt)
 remove_token_query_param()
